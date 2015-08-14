5bcecd94268fd4d0e9c2c8f15b4e5ee0261970a5
levansuper <l.basharuli@gmail.com>
2015-08-14 14:44:43 +0400

    aded keywords

--- readableStreamClone.js
+++ readableStreamClone.js
@@ -1,11 +1,10 @@
 var util = require("util");
 var Readable = require('stream').Readable;
-var Buffer = require("buffer");
 
 var ReadableStream = function(readableStream, options) {
     var me = this;
     Readable.call(me, options);
-    
+
     readableStream.on("data", function(chunk) {
         me.push(chunk);
     });
@@ -13,15 +12,14 @@ var ReadableStream = function(readableStream, options) {
     readableStream.on('end', function() {
         me.push(null);
     });
-    
-    readableStream.on("error", function(err){
+
+    readableStream.on("error", function(err) {
         me.push(err);
-    });    
-    me._read = function(){}
+    });
+    me._read = function() {
+    };
 };
 
 util.inherits(ReadableStream, Readable);
-
-
 
 module.exports = ReadableStream;

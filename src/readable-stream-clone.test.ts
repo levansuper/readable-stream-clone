a7cd0c781104fe444cf326824e5efcf11c1333d3
Perry Mitchell <perry@perrymitchell.net>
2024-02-25 15:08:30 +0200

    Update formatting preferences as per PR

--- src/readable-stream-clone.test.ts
+++ src/readable-stream-clone.test.ts
@@ -8,35 +8,35 @@ const fileName1 = path + `/text1.txt`;
 const fileName2 = path + `/text2.txt`;
 
 describe('readable-stream-clone', () => {
-    beforeEach(async () => {
-        await fs.rm(path, { recursive: true, force: true });
-        await fs.mkdir(path);
-    });
+	beforeEach(async () => {
+		await fs.rm(path, { recursive: true, force: true });
+		await fs.mkdir(path);
+	});
 
-    afterEach(async () => {
-        await fs.rm(path, { recursive: true, force: true });
-    });
+	afterEach(async () => {
+		await fs.rm(path, { recursive: true, force: true });
+	});
 
-    it('should pipe readable into writable stream', async () => {
-        const testStr = 'testStr';
-        const buffer = Buffer.from(testStr, 'utf8');
+	it('should pipe readable into writable stream', async () => {
+		const testStr = 'testStr';
+		const buffer = Buffer.from(testStr, 'utf8');
 
-        const readStream = Readable.from(buffer);
-        const readClone1 = new ReadableStreamClone(readStream);
-        const readClone2 = new ReadableStreamClone(readStream);
+		const readStream = Readable.from(buffer);
+		const readClone1 = new ReadableStreamClone(readStream);
+		const readClone2 = new ReadableStreamClone(readStream);
 
-        const writeStream1 = fs.createWriteStream(fileName1);
-        const writeStream2 = fs.createWriteStream(fileName2);
+		const writeStream1 = fs.createWriteStream(fileName1);
+		const writeStream2 = fs.createWriteStream(fileName2);
 
-        readClone1.pipe(writeStream1);
-        readClone2.pipe(writeStream2);
+		readClone1.pipe(writeStream1);
+		readClone2.pipe(writeStream2);
 
-        await promisifyWriteStreams([writeStream1, writeStream2]);
+		await promisifyWriteStreams([writeStream1, writeStream2]);
 
-        const result1 = fs.readFileSync(fileName1, 'utf8');
-        const result2 = fs.readFileSync(fileName2, 'utf8');
+		const result1 = fs.readFileSync(fileName1, 'utf8');
+		const result2 = fs.readFileSync(fileName2, 'utf8');
 
-        expect(result1).toEqual(testStr);
-        expect(result2).toEqual(testStr);
-    });
+		expect(result1).toEqual(testStr);
+		expect(result2).toEqual(testStr);
+	});
 });
--- src/readable-stream-clone.ts
+++ src/readable-stream-clone.ts
@@ -1,38 +1,38 @@
 import { Readable, Writable, ReadableOptions } from 'stream';
 
 export class ReadableStreamClone extends Readable {
-    constructor(readableStream: Readable, options?: ReadableOptions) {
-        super(options);
-        readableStream.on('data', chunk => {
-            this.push(chunk);
-        });
+	constructor(readableStream: Readable, options?: ReadableOptions) {
+		super(options);
+		readableStream.on('data', chunk => {
+			this.push(chunk);
+		});
 
-        readableStream.on('end', () => {
-            this.push(null);
-        });
+		readableStream.on('end', () => {
+			this.push(null);
+		});
 
-        readableStream.on('error', err => {
-            this.emit('error', err);
-        });
-    }
-    public _read() {}
+		readableStream.on('error', err => {
+			this.emit('error', err);
+		});
+	}
+	public _read() {}
 }
 
 export const promisifyWriteStreams = async (writableStreams: Writable[]) => {
-    return Promise.all(
-        writableStreams.map((writable: Writable) => {
-            return promisifyWriteStream(writable);
-        })
-    );
+	return Promise.all(
+		writableStreams.map((writable: Writable) => {
+			return promisifyWriteStream(writable);
+		})
+	);
 };
 
 export const promisifyWriteStream = async (writableStream: Writable) => {
-    return new Promise((resolve, reject) => {
-        writableStream.on('finish', () => {
-            resolve(null);
-        });
-        writableStream.on('error', err => {
-            reject(err);
-        });
-    });
+	return new Promise((resolve, reject) => {
+		writableStream.on('finish', () => {
+			resolve(null);
+		});
+		writableStream.on('error', err => {
+			reject(err);
+		});
+	});
 };
